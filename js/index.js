09862fa858f65ccdaffb2060eae901be8a557e88
Kevin Sims <kevinsims@Kevins-MacBook-Pro.local>
2019-05-14 09:01:39 -0700

    mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.querySelector('nav').addEventListener('mouseover', 
+(event) => {event.target.style.color = "orange"
+setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
